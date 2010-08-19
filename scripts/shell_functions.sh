6cd77d51161aec448ab38dbae1a73338580407fe
David Soulayrol <david.soulayrol@gmail.com>
2010-08-19 15:21:21 +0200

    Added some more personal tweaks in scripts/shell_functions.sh.

--- scripts/shell_functions.sh
+++ scripts/shell_functions.sh
@@ -1,10 +1,34 @@
 # This script should be sourced by .bashrc or equivalent.
 
-search_mail()
-{
+# make use of both CPU when compiling.
+export MAKEFLAGS="-j`ls -1 /sys/devices/system/cpu/cpu* | wc -l`"
+
+# dh_make environment.
+export DEBEMAIL=david.soulayrol@gmail.com
+export DEBFULLNAME="David Soulayrol"
+
+# Personal environment.
+export TERM=linux
+export EDITOR="emacsclient -c -a emacs"
+export VISUAL="emacsclient -c -a emacs"
+
+# open mutt on a mairix search results
+search_mail() {
     mairix -t $*
 
     if test -n "`ls ~/mail/mairix/cur`" -o -n "`ls ~/mail/mairix/new`"; then
         mutt -f =mairix;
     fi
  }
+
+# make svn diff more friendly
+svndiff () {
+    svn diff -x -w "${@}" | colordiff | less -R
+}
+
+# http://chris-lamb.co.uk/2010/04/22/locating-source-any-python-module/
+cdp () {
+    cd "$(python -c "import os.path as _, ${1}; \
+        print _.dirname(_.realpath(${1}.__file__[:-1]))"
+  )"
+}
