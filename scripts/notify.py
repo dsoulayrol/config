8d2b0548d7292b025ddafe2c9fc38d9c9ebd659e
David Soulayrol <david.soulayrol@gmail.com>
2011-04-27 16:52:56 +0200

    Improved notify.py to search for the DBUS session, so that it can be called from cron.
    It's not perfect but enough for a standard desktop. It could be
    improved with rules to select the correct session to take if more than
    one are present (eg. the last opened).

--- scripts/notify.py
+++ scripts/notify.py
@@ -6,10 +6,28 @@
 # it will stay displayed until acknowledged.
 
 import dbus
+import os
 import sys
 
-sysbus = dbus.SessionBus()
-notify_obj = sysbus.get_object('org.freedesktop.Notifications',
-                              '/org/freedesktop/Notifications')
+DBUS_DIR = '/home/david/.dbus/session-bus/'
+
+def load_session():
+    # For now, take the first session found.
+    sessions = os.listdir(DBUS_DIR)
+    if len(sessions):
+        with open(os.path.join(DBUS_DIR, sessions[0]), 'r') as f:
+            for line in f:
+                if line.startswith('DBUS'):
+                    key, value = line.split('=', 1)
+                    os.environ[key] = value[:-1]
+
+def get_session_bus():
+    if not os.environ.has_key('DBUS_SESSION_BUS_ADDRESS'):
+        load_session()
+    return dbus.SessionBus()
+
+
+notify_obj = get_session_bus().get_object('org.freedesktop.Notifications',
+                                          '/org/freedesktop/Notifications')
 notify_if = dbus.Interface(notify_obj, 'org.freedesktop.Notifications')
 notify_if.Notify('notify.py', 0, '', sys.argv[2], sys.argv[3], [], {}, int(sys.argv[1]))
